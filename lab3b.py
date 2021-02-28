822f60cc8bcd56582e453c93c7ede075d02350cd
Connor Patrick Daly <classcda@lnxsrv09.seas.ucla.edu>
2021-02-28 15:36:17 -0800

    it passes the sanity check now but idk if its correct at all. Some parts especially don't make sense at all. I left some comments to try to say what I was doing

--- lab3b.py
+++ lab3b.py
@@ -8,18 +8,25 @@ Ifrees = set([])
 Dirents = []
 Indirects = []
 Groups = []
-occupiedBlocks = set([])
+#occupiedBlocks = set([])
+#tracks all the blocks that are referenced
 referencedBlocks = dict([])
+#counts how many directories an inode says its referenced by
+linkCounts = dict([])
+#counts how many directories are referenced by an inode
+linksFound = dict([])
+#for each child directory stores their parent
+childParentDirs = dict([])
 
 def main():
    if len(sys.argv) != 2:
-      sys.stderr.write("please use one and only one argument\n")
+      sys.stderr.write("please use one and only one argument")
       exit(1)
    #to check if file is valid
    try:
       csvFile = open(sys.argv[1])
    except:
-      sys.stderr.write("Error: couldn't open specified file\n")
+      sys.stderr.write("Error: couldn't open specified file")
       exit(1)
    
    #Go through the file and sort the different lines into lists
@@ -41,21 +48,28 @@ def main():
             Ibitmap = int(line[7])
             reserved = int(line[8]) + inodeSize*int(line[3])/blockSize
          elif identifier == 'BFREE':
-            Bfrees.add(line[1])
+            Bfrees.add(int(line[1]))
          elif identifier == 'IFREE':
-            Ifrees.add(line[1]) 
+            Ifrees.add(int(line[1])) 
          elif identifier == 'DIRENT':
             Dirents.append(line)
          elif identifier == 'INODE':
             Inodes.append(line)
          elif identifier == 'INDIRECT':
             Indirects.append(line)
+            if line[5] in referencedBlocks:
+               referencedBlocks[int(line[5])] += 1
+            else:
+               referencedBlocks[int(line[5])] = 1
   
    #Inode part
    for line in Inodes:
-      if line[1] in Ifrees:
-         print("Allocated INODE " + line[1] + " ON FREELIST\n")
-      Ifrees.add(line[1])
+      #might have to check if the mode is 0 of the inodes
+      if int(line[1]) in Ifrees:
+         print("ALLOCATED INODE " + line[1] + " ON FREELIST")
+         Ifrees.remove(int(line[1]))
+      #Ifrees.add(int(line[1]))
+      linkCounts.update({int(line[1]): int(line[6])}) 
       if line[2] != "s":
          for i in range(0, 15):
             blockNum = int(line[12+i])
@@ -84,16 +98,84 @@ def main():
                elif i == 13:
                   print("RESERVED DOUBLE INDIRECT BLOCK " + str(blockNum) + " IN INODE " + line[1] + " AT OFFSET 268")
                elif i == 14:
-                  print("RESERVED TRIPLE INDIRECT BLOCK " + str(blockNum) + " IN INODE " + line[1] + " AT OFFSET 65804")                       
+                  print("RESERVED TRIPLE INDIRECT BLOCK " + str(blockNum) + " IN INODE " + line[1] + " AT OFFSET 65804")                    
    
+   #check for allocated blocks in freelist
+   for i in Bfrees:
+      if i in referencedBlocks:
+         print("ALLOCATED BLOCK " + str(i) + " ON FREELIST")
+      else: 
+         #print(i)
+         referencedBlocks[i] = 1
+         #print(i in referencedBlocks)
+      
+   #check unreferenced blocks
+   for i in range(reserved +1, maxBlockNum):
+      if not int(i) in referencedBlocks:
+         r = 0
+         print("UNREFERENCED BLOCK " + str(i))
+         #print(referencedBlocks[i])
+
+   #directory part
+   for line in Dirents:
+      inodeNum = int(line[3])
+      if inodeNum in linksFound:
+         linksFound[inodeNum] += 1
+      else:
+         linksFound.update({inodeNum : 1})
+      if inodeNum < 0 or inodeNum > numInodes:
+         print("DIRECTORY INODE " + line[1] + " NAME " + line[6] + " INVALID INODE " + str(inodeNum))
+      #might have to check the inodes and if it has mode == 0
+      elif inodeNum in Ifrees:
+         print("DIRECTORY INODE " + line[1] + " NAME " + line[6] + " UNALLOCATED INODE " + str(inodeNum))
+      elif line[6] == "'.'" and int(line[1]) != inodeNum:
+         print("DIRECTORY INODE " + line[1] + " NAME " + line[6] + " LINK TO INODE " + str(inodeNum) + " SHOULD BE " + line[1])
+      elif line[6] != "'.'" and line[6] != "'..'" :
+         #print(str(inodeNum) + " " + line[1])
+         childParentDirs.update({int(inodeNum) : int(line[1])})
+      #need to have the checking for parent directory part
+      #elif line[6] == "'..'" and int(line[1]) != inodeNum:
+         #print("DIRECTORY INODE " + line[1] + " NAME " + line[6] + " LINK TO INODE " + str(inodeNum) + " SHOULD BE " + line[1])
+
+   #check for .. links correct
+   for line in Dirents:
+      if line[6] == "'..'":
+         #print(line)
+         #idk what to do if its not, i dont think that could happen - for some reason sometimes 2 is the parent even though idk why so I'm assuming if parent isn't given that it's 2
+         if int(line[1]) in childParentDirs:
+            #print(str(childParentDirs[int(line[1])]) + " " + line[3])
+            if childParentDirs[int(line[1])] != int(line[3]):
+               print("DIRECTORY INODE " + line[1] + " NAME " + line[6] + " LINK TO INODE " + line[3] + " SHOULD BE " + str(childParentDirs[int(line[1])]))
+         elif int(line[3]) != 2:
+            print("DIRECTORY INODE " + line[1] + " NAME " + line[6] + " LINK TO INODE " + line[3] + " SHOULD BE 2")
+
+   #check for numLinks
+   for node in linkCounts:
+      if not node in linksFound:
+         print("INODE " + str(node) + " HAS 0 LINKS BUT LINKCOUNT IS " + str(linkCounts[node]))
+      elif linkCounts[node] != linksFound[node]:
+         print("INODE " + str(node) + " HAS " + str(linksFound[node]) + " LINKS BUT LINKCOUNT IS " + str(linkCounts[node]))
+   #check duplicates
+   for line in Inodes:
+      #for checking unallocated node not in list
+      Ifrees.add(int(line[1]))
+      for i in range(0, 15):
+         blockNum = int(line[12+i])
+         if blockNum != 0 and blockNum in referencedBlocks:
+            if referencedBlocks[blockNum] > 1:
+               if i < 12:
+                  print("DUPLICATE BLOCK " + str(blockNum) + " IN INODE " + line[1] + " AT OFFSET " + str(i))
+               elif i == 12:
+                  print("DUPLICATE INDIRECT BLOCK " + str(blockNum) + " IN INODE " + line[1] + " AT OFFSET 12")
+               elif i == 13:
+                  print("DUPLICATE DOUBLE INDIRECT BLOCK " + str(blockNum) + " IN INODE " + line[1] + " AT OFFSET 268")
+               elif i == 14:
+                  print("DUPLICATE TRIPLE INDIRECT BLOCK " + str(blockNum) + " IN INODE " + line[1] + " AT OFFSET 65804")
+
    #check for unallocated Inodes
    for i in range(firstNonReservedInode, numInodes+1):
-      if not str(i) in Ifrees:
+      if not i in Ifrees:
          print("UNALLOCATED INODE " + str(i) + " NOT ON FREELIST")
-
-   #check for duplicate references to blocks or unreferenced blocks
-
-   #directory part        
 
 if __name__ == "__main__":
     main()
